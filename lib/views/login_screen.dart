90457826f3ae806157479856470edd8a066921bc
itsjeanem <jeanem@duck.com>
2025-12-29 22:00:12 +0000

    feat: update login screen to navigate to home and improve layout

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../views/home_screen.dart';
 import '../widgets/login_form.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,70 +15,88 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const SizedBox(height: 48),
-          const Column(
-            children: [
-              Text(
-                'Bienvenue',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 4),
-              Text(
-                'Connectez-vous pour accéder à votre espace',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [SizedBox(height: 48), LoginForm()],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 48),
+            const Column(
               children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/signup');
-                  },
-                  child: const Text(
-                    'Créer un compte',
-                    style: TextStyle(
-                      color: Color(0xFFF97316),
-                      fontWeight: FontWeight.w600,
-                    ),
+                Text(
+                  'Bienvenue',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
                   ),
                 ),
-                const Text('Ou'),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/invitation');
-                  },
-                  child: const Text(
-                    "J'ai un code d'invitation locataire",
-                    style: TextStyle(
-                      color: Color(0xFF2563EB),
-                      fontWeight: FontWeight.w600,
-                    ),
+                SizedBox(height: 4),
+                Text(
+                  'Connectez-vous pour accéder à votre espace',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [SizedBox(height: 48), LoginForm()],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                      );
+                    },
+                    child: const Text(
+                      "Go to home",
+                      style: TextStyle(
+                        color: Color(0xFF2563EB),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: const Text(
+                      'Créer un compte',
+                      style: TextStyle(
+                        color: Color(0xFFF97316),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  const Text('Ou'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/invitation');
+                    },
+                    child: const Text(
+                      "J'ai un code d'invitation locataire",
+                      style: TextStyle(
+                        color: Color(0xFF2563EB),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
