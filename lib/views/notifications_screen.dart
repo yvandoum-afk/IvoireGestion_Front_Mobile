90390df9b044fd2471d0948e32dae883b7d1de57
itsjeanem <jeanem@duck.com>
2025-12-30 18:09:33 +0000

    feat: update NotificationsScreen layout with no notifiications message

--- lib/views/notifications_screen.dart
+++ lib/views/notifications_screen.dart
@@ -13,8 +13,24 @@ class NotificationsScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: const Center(
-        child: Text('Notifications Screen'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.notifications_none_outlined,
+                  size: 64, color: Colors.grey.shade400),
+              const Text(
+                "Centre de notifications",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 12),
+              const Text("Restez informé de tous les événements importants"),
+            ],
+          ),
+        ),
       ),
     );
   }
